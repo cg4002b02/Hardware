a449553e37def897b1f7680960b312e15373bb0a
Samueltansw <samuetan123@gmail.com>
2025-03-05 18:51:33 +0800

    clean up gun code

--- components/gun/gun.ino
+++ components/gun/gun.ino
@@ -29,8 +29,6 @@ CRC8 crc(0x07);
 
 void (*reset) (void) = 0;
 
-bool syncMotion = false;
-
 enum ProtoState { DISCONNECTED, HANDSHAKE_INITIATED, WAITING_FOR_ACK, CONFIRMED};
 ProtoState protoState = DISCONNECTED;
 
@@ -69,19 +67,6 @@ const unsigned long TIMEOUT_VALHIT = 750;
 unsigned long lastHitSendTime = 0;
 bool lastsentHitindex = true;
 
-// Sensor dummy data (randomise between 3 for testing)
-int accX[][1] = {{1}, {5}, {-3}};
-int accY[][1] = {{2}, {6}, {-4}};
-int accZ[][1] = {{4}, {7}, {-2}};
-int gyrX[][1] = {{100}, {-200}, {50}};
-int gyrY[][1] = {{-150}, {250}, {-100}};
-int gyrZ[][1] = {{100}, {-300}, {200}};
-//int yaw[][1] = {{340}, {90}, {180}};
-//int pitch[][1] = {{200}, {150}, {50}};
-//int roll[][1] = {{134}, {45}, {90}};
-
-// MPU6050 mpu;
-
 struct gameState_t
 {
   int health;
@@ -102,7 +87,6 @@ const float ACCEL_THRESHOLD = 12;
 
 bool isMotion = false;
 int imuWindowIndex = IMU_WINDOW_SIZE;
-imuData_t imuDataArray[IMU_WINDOW_SIZE];
 
 struct Datapacket {
   int8_t type; //bhhhhhhhbbbh
@@ -133,21 +117,6 @@ struct Ackpacket {
   int16_t checksum;
 };
 
-struct Hitpacket {
-  int8_t type;
-  int8_t hit_status;
-  int16_t health_status;
-  int16_t padding_1;
-  int16_t padding_2;
-  int16_t padding_3;
-  int16_t padding_4;
-  int16_t padding_5;
-  int16_t padding_6;
-  int16_t padding_7;
-  int8_t padding_8;
-  int8_t checksum;
-};
-
 struct Gunpacket {
   int8_t type;
   int8_t ammo_state;
@@ -164,7 +133,6 @@ struct Gunpacket {
 };
 
 Gunpacket lastGunpacket; 
-Hitpacket lastHitpacket;
 
 uint8_t calculateCRC8(uint8_t *data, int len) {
   crc.restart();
@@ -260,7 +228,7 @@ void shootAmmo(unsigned long currentMillis)
     if (gameState.ammo > 0)
     {
       // Serial.println("shoot");
-      sendgun();
+      sendGun();
 
       hasSentGun = true;
       hasAcknowledgedGun = false;
@@ -302,25 +270,25 @@ void updateLed()
   FastLED.show();
 }
 
-void getGameState()
-{
-  if (Serial.available() > 0)
-  {
-    String input = Serial.readStringUntil('\n');
-    int separatorIndex = input.indexOf(',');
-    if (separatorIndex != -1)
-    {
-      String healthStr = input.substring(0, separatorIndex);
-      String ammoStr = input.substring(separatorIndex + 1);
-      gameState.health = healthStr.toInt();
-      gameState.ammo = ammoStr.toInt();
-      Serial.print("Updated gameState - Health = ");
-      Serial.print(gameState.health);
-      Serial.print(", Ammo = ");
-      Serial.println(gameState.ammo);
-    }
-  }
-}
+// void getGameState()
+// {
+//   if (Serial.available() > 0)
+//   {
+//     String input = Serial.readStringUntil('\n');
+//     int separatorIndex = input.indexOf(',');
+//     if (separatorIndex != -1)
+//     {
+//       String healthStr = input.substring(0, separatorIndex);
+//       String ammoStr = input.substring(separatorIndex + 1);
+//       gameState.health = healthStr.toInt();
+//       gameState.ammo = ammoStr.toInt();
+//       Serial.print("Updated gameState - Health = ");
+//       Serial.print(gameState.health);
+//       Serial.print(", Ammo = ");
+//       Serial.println(gameState.ammo);
+//     }
+//   }
+// }
 
 // Loop: first filter handshake traffic; only once CONFIRMED, process data
 
@@ -344,91 +312,35 @@ bool isMotionDetected(imuData_t imuData)
   return (accelMag > ACCEL_THRESHOLD);
 }
 
-// void loop()
-// {
-//   // button logic
-//   shootAmmo();
-//   updateLed();
-//   Serial.println(digitalRead(BUTTON_PIN) == HIGH);
+void initiateHandshake() {
+  if (Serial.available()) {
+    incoming = Serial.read();
+    switch(incoming) {
+      case HANDSHAKE:
+        protoState = HANDSHAKE_INITIATED;
+        if (protoState == DISCONNECTED || protoState == HANDSHAKE_INITIATED) {
+          sendAck();
+          protoState = WAITING_FOR_ACK;
+        }
+        break;
+      case 'a':
+        if (protoState == WAITING_FOR_ACK) {
+          protoState = CONFIRMED;
+        }
+        break;
+      case 'r':  // Reset command from Python, if any.
+        protoState = DISCONNECTED;
+        reset();
+        break;
 
-//   // auto reload
-//   if (gameState.ammo == 0)
-//   {
-//     Serial.println("Reloading...");
-//     delay(1500);
-//     Serial.println("Done reloading");
-//     gameState.ammo = 6;
-//   }
-// }
-
-void loop(){
-  char incoming;
-  
-  // If handshake is not confirmed, process only handshake-related bytes.
-  if (protoState != CONFIRMED) {
-    while (Serial.available()) {
-      incoming = Serial.read();
-      switch(incoming) {
-        case HANDSHAKE:
-          protoState = HANDSHAKE_INITIATED;
-          if (protoState == DISCONNECTED || protoState == HANDSHAKE_INITIATED) {
-            sendack();
-            protoState = WAITING_FOR_ACK;
-          }
-          break;
-        case 'a':
-          if (protoState == WAITING_FOR_ACK) {
-            protoState = CONFIRMED;
-          }
-          break;
-        case 'r':  // Reset command from Python, if any.
-          protoState = DISCONNECTED;
-          reset();
-          break;
-        case 'l':
-          syncMotion = true;
-
-        default:
-          // Ignore any other data.
-          break;
-      }
+      default:
+        // Ignore any other data.
+        break;
     }
-    // Do nothing else until handshake is CONFIRMED.
-    return;
   }
+}
 
-
-  imuData_t imuData = getImuReadings();
-
-  isMotion = isMotionDetected(imuData);
-
-  if (imuWindowIndex == IMU_WINDOW_SIZE && isMotion)
-  {
-    imuWindowIndex = 0;
-  }
-
-  // if (protoState == CONFIRMED) {
-  //   // mimic receiving gameState from game engine
-  //   getGameState();
-  // }
-
-  // send data
-  senddata(imuData);
-
-  // button logic
-  // shootAmmo();
-  updateLed();
-
-  // auto reload
-  if (gameState.ammo == 0)
-  {
-    // Serial.println("Reloading...");
-    delay(500);
-    // Serial.println("Done reloading");
-    gameState.ammo = 6;
-  }
-  
-  // Now that handshake is CONFIRMED, process update packets FROM python
+void updateGameState() {
   if (Serial.available()) {
     incoming = Serial.read();
     
@@ -451,13 +363,13 @@ void loop(){
               int value = (updateBuffer[2] << 8) | updateBuffer[1];  // little-endian
               switch (updateBuffer[0]) {
                 case 'B':  
-                  gameState.ammo = value;
+                  ammo = value;
                   break;
                 case 'E':  
-                  gameState.health = value;
+                  points = value;
                   break;
                 case 'W':  
-                  gameState.ammo = 6;
+                  ammo = 6;
                   break;
               }
               currentUpdateState = IDLE;
@@ -484,78 +396,87 @@ void loop(){
       }
     }
   }
+}
+
+
+void loop(){
+  char incoming;
   
-  // Process periodic tasks (sending sensor data, gun/hit packets, etc.) if handshake is CONFIRMED.
+  // If handshake is not confirmed, process only handshake-related bytes.
+  if (protoState != CONFIRMED) {
+    initiateHandshake();
+    // Do nothing else until handshake is CONFIRMED.
+    return;
+  }
+
+  // Assert: protoState is CONFIRMED
+  // Now that handshake is CONFIRMED, process update packets FROM python
+  updateGameState();
+
+  imuData_t imuData = getImuReadings();
+
+  isMotion = isMotionDetected(imuData);
+
+  if (imuWindowIndex == IMU_WINDOW_SIZE && isMotion)
+  {
+    imuWindowIndex = 0;
+  }
+
+  // send data
+  if (imuWindowIndex < IMU_WINDOW_SIZE) {
+    // Serial.print("sending packet #");
+    // Serial.println(imuWindowIndex);
+    sendData(imuData);
+
+    ++imuWindowIndex;      
+  }
+
+  // temp auto reload
+  if (gameState.ammo == 0)
+  {
+    // Serial.println("Reloading...");
+    delay(500);
+    // Serial.println("Done reloading");
+    gameState.ammo = 6;
+  }
+
   unsigned long currentMillis = millis();
 
-  if (protoState == CONFIRMED && !hasSentGun) {
-      // sendgun();
-      // if (ammo > 0){
-      //   ammo -= 1;
-      // } else {
-      //   ammo = 0;
-      // }
-      shootAmmo(currentMillis);
-      // lastGunSendTime = currentMillis;
-      // hasSentGun = true;
-      // hasAcknowledgedGun = false;
-      // timeoutStart = currentMillis;
+  if (!hasSentGun) {
+    shootAmmo(currentMillis);
   }
+  updateLed();
 
-  if (protoState == CONFIRMED && hasSentGun && !hasAcknowledgedGun && (currentMillis - timeoutStart >= TIMEOUT_VAL)) {
+  if (hasSentGun && !hasAcknowledgedGun && (currentMillis - timeoutStart >= TIMEOUT_VAL)) {
       resendGunpacket();
       timeoutStart = currentMillis;
-      // lastGunSendTime = currentMillis;
   }
-
-  // if (protoState == CONFIRMED && !hasSentHit && (currentMillis - lastHitSendTime >= 1500)) {
-  //     sendhit();
-  //     lastHitSendTime = currentMillis;
-  //     hasSentHit = true;
-  //     hasAcknowledgedHit = false;
-  //     timeoutStartHit = lastHitSendTime;
-  // }
-
-  // if (protoState == CONFIRMED && hasSentHit && !hasAcknowledgedHit && (currentMillis - timeoutStartHit >= TIMEOUT_VALHIT)) {
-  //     resendHitpacket();
-  //     timeoutStartHit = currentMillis;
-  //     lastHitSendTime = currentMillis;
-  // }
-
+  
   delay(50); // period = 50ms = 20Hz -> 20 samples per second
 }
 
 //---------------------------------------------------------
 // Packet Sending Functions
 //---------------------------------------------------------
-void senddata(imuData_t imuData){
-
-  if (protoState == CONFIRMED && imuWindowIndex < IMU_WINDOW_SIZE) {
-    // Serial.print("sending packet #");
-    // Serial.println(imuWindowIndex);
-
-    Datapacket packet;
-    packet.type = IMU_DATA;
-    int index = random(0, 3); //toggle dummy data
-    packet.aX = imuData.ax;
-    packet.aY = imuData.ay;
-    packet.aZ = imuData.az;
-    packet.gX = imuData.gx;
-    packet.gY = imuData.gy;
-    packet.gZ = imuData.gz;
-    packet.y = 0;
-    packet.p = 0;
-    packet.r = 0;
-    packet.start_move = 0; 
-    packet.checksum = getChecksum(packet); 
-    Serial.write((uint8_t *)&packet, sizeof(packet));
-
-    ++imuWindowIndex;      
-  }
-
+void sendData(imuData_t imuData){
+  Datapacket packet;
+  packet.type = IMU_DATA;
+  int index = random(0, 3); //toggle dummy data
+  packet.aX = imuData.ax;
+  packet.aY = imuData.ay;
+  packet.aZ = imuData.az;
+  packet.gX = imuData.gx;
+  packet.gY = imuData.gy;
+  packet.gZ = imuData.gz;
+  packet.y = 0;
+  packet.p = 0;
+  packet.r = 0;
+  packet.start_move = 0; 
+  packet.checksum = getChecksum(packet); 
+  Serial.write((uint8_t *)&packet, sizeof(packet));
 }
 
-void sendack() {
+void sendAck() {
   Ackpacket packet;
   packet.type = ACK;
   packet.padding_1 = 0;
@@ -571,24 +492,7 @@ void sendack() {
   Serial.write((uint8_t *)&packet, sizeof(packet));
 }
 
-void sendhit() {
-  int index = random(0, 3); //toggle dummy data
-  lastHitpacket.type = HIT;
-  lastHitpacket.hit_status = 0;
-  lastHitpacket.health_status = points; 
-  lastHitpacket.padding_1 = 0;
-  lastHitpacket.padding_2 = 0;
-  lastHitpacket.padding_3 = 0;
-  lastHitpacket.padding_4 = 0;
-  lastHitpacket.padding_5 = 0;
-  lastHitpacket.padding_6 = 0;
-  lastHitpacket.padding_7 = 0;
-  lastHitpacket.padding_8 = 0;
-  lastHitpacket.checksum = getHitChecksum(lastHitpacket);
-  Serial.write((uint8_t *)&lastHitpacket, sizeof(lastHitpacket));
-}
-
-void sendgun() {
+void sendGun() {
   int index = random(0, 2); //toggle dummy data
   lastGunpacket.type = GUN;
   lastGunpacket.ammo_state = gameState.ammo;
@@ -609,24 +513,12 @@ void resendGunpacket() {
   Serial.write((uint8_t *)&lastGunpacket, sizeof(lastGunpacket));
 }
 
-void resendHitpacket() {
-  Serial.write((uint8_t *)&lastHitpacket, sizeof(lastHitpacket));
-}
-
 long getChecksum(Datapacket packet){
   return packet.type ^ packet.aX ^ packet.aY ^ packet.aZ ^ packet.gX ^ packet.gY ^ packet.gZ ^ packet.y ^ packet.p ^ packet.r ^ packet.start_move;
 }
 
 long getAckChecksum(Ackpacket packet){
   return packet.type ^ packet.padding_1 ^ packet.padding_2 ^ packet.padding_3 ^ packet.padding_4 ^ packet.padding_5 ^ packet.padding_6 ^ packet.padding_7 ^ packet.padding_8 ^ packet.padding_9;
-}
-
-long getHitChecksum(Hitpacket lastHitpacket) {
-    uint8_t *data = (uint8_t *)&lastHitpacket;
-    int len = sizeof(Hitpacket) - sizeof(lastHitpacket.checksum);
-    crc.restart();
-    crc.add(data, len);
-    return (long) crc.getCRC();
 }
 
 long getGunChecksum(Gunpacket lastGunpacket) {
@@ -636,302 +528,3 @@ long getGunChecksum(Gunpacket lastGunpacket) {
     crc.add(data, len);
     return (long) crc.getCRC();
 }
-
-
-/////////////////////////////////////////////////////
-
-
-// #include <MPU6050.h>
-// #include <Arduino.h>
-// #include <FastLED.h>
-// #include <IRremote.h>
-// #include <Wire.h>
-
-// #define LED_PIN 5
-// #define BUTTON_PIN 2
-// #define IR_RECEIVER_PIN 4
-// #define IR_EMITTER_PIN 3
-
-// #define NUM_LEDS 3
-// #define LED_BRIGHTNESS 10
-// // #define WINDOW_SIZE 5 // Sliding window size
-// #define IMU_WINDOW_SIZE 20
-
-// MPU6050 mpu;
-// CRGB leds[NUM_LEDS];
-
-// Circular buffer arrays
-// double accelBuffer[WINDOW_SIZE] = {0};
-// double gyroBuffer[WINDOW_SIZE] = {0};
-// int bufferIndex = 0;
-// bool bufferFilled = false;
-
-// struct gameState_t
-// {
-//   int health;
-//   int ammo;
-// };
-
-// gameState_t gameState = {100, 6};
-
-// struct imuData_t
-// {
-//   double ax, ay, az; // Acceleration
-//   double gx, gy, gz; // Gyroscope
-// };
-
-// bool buttonPressed = false;
-// const float GRYO_THRESHOLD = 112;
-// const float ACCEL_THRESHOLD = 12;
-
-// bool isMotion = false;
-// int isMotionCounter = 0;
-// int imuWindowIndex = IMU_WINDOW_SIZE;
-// imuData_t imuDataArray[IMU_WINDOW_SIZE];
-
-// void setup()
-// {
-
-//   Serial.begin(115200);
-
-//   // LED strip setup
-//   setupLed();
-
-//   // MPU setup
-//   setupMpu();
-
-//   // IR transmitter
-//   setupIrTransmitter();
-
-//   // Button setup
-//   pinMode(BUTTON_PIN, INPUT);
-// }
-
-// void setupLed()
-// {
-//   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-//   FastLED.setBrightness(LED_BRIGHTNESS);
-// }
-
-// void setupMpu()
-// {
-//   Serial.println("Init MPU6050");
-//   while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-//   {
-//     Serial.println("No MPU");
-//     delay(500);
-//   }
-
-//   mpu.calibrateGyro();
-//   mpu.setThreshold(3);
-//   mpu.setDLPFMode(MPU6050_DLPF_2); // mode 2 for general movement detection
-// }
-
-// void setupIrTransmitter()
-// {
-//   IrSender.begin(IR_EMITTER_PIN);
-//   Serial.println("IR Transmitter initialized.");
-// }
-
-// void sendIrSignal()
-// {
-//   uint8_t address = 0x11;
-//   uint8_t command = 0x22;
-
-//   Serial.println("Sending IR signal...");
-//   IrSender.sendNEC(address, command, 0); // NEC protocol
-//   Serial.print("Sent NEC IR Signal - Address = ");
-//   Serial.print(address, HEX);
-//   Serial.print(", Command = ");
-//   Serial.println(command, HEX);
-// }
-
-// float computeMagnitude(float x, float y, float z)
-// {
-//   return sqrt(x * x + y * y + z * z);
-// }
-
-// float prevAccel[3] = {2.73, 0.53, 8.25}; // AccelXnorm:2.73, AccelYnorm:0.53, AccelZnorm:8.25
-// const float ACCEL_THRESHOLD_2 = 9;
-// bool isMotionDetected2(imuData_t imuData)
-// {
-//   // Compute magnitude of acceleration & gyro data
-//   double accelMag = computeMagnitude(imuData.ax - prevAccel[0], imuData.ay - prevAccel[1], imuData.az - prevAccel[2]);
-
-//   prevAccel[0] = imuData.ax;
-//   prevAccel[1] = imuData.ay;
-//   prevAccel[2] = imuData.az;
-
-//   // Check if motion is detected
-//   return (accelMag > ACCEL_THRESHOLD_2);
-// }
-
-// imuData_t getImuReadings()
-// {
-//   imuData_t imuData;
-//   Vector normGyro = mpu.readNormalizeGyro();
-//   Vector normAccel = mpu.readNormalizeAccel();
-
-//   imuData.gx = normGyro.XAxis;
-//   imuData.gy = normGyro.YAxis;
-//   imuData.gz = normGyro.ZAxis;
-//   imuData.ax = normAccel.XAxis;
-//   imuData.ay = normAccel.YAxis;
-//   imuData.az = normAccel.ZAxis;
-
-//   return imuData;
-// }
-
-// void shootAmmo()
-// {
-//   if (!buttonPressed && digitalRead(BUTTON_PIN) == HIGH)
-//   {
-//     if (gameState.ammo > 0)
-//     {
-//       Serial.println("shoot");
-//       buttonPressed = true;
-//       sendIrSignal();
-//       --gameState.ammo;
-//     }
-//     else
-//     {
-//       Serial.println("No ammo left. Please reload...");
-//     }
-//   }
-//   if (digitalRead(BUTTON_PIN) == LOW)
-//   {
-//     buttonPressed = false;
-//   }
-// }
-
-// void updateLed()
-// {
-//   int numFullLed = gameState.ammo / 2;
-//   bool hasHalfLed = gameState.ammo % 2 == 1;
-
-//   for (int i = 0; i < numFullLed; ++i)
-//   {
-//     leds[i] = CRGB(0, 255, 0);
-//   }
-//   for (int j = numFullLed; j < NUM_LEDS; ++j)
-//   {
-//     leds[j] = CRGB(0, 0, 0);
-//   }
-//   if (hasHalfLed)
-//   {
-//     leds[numFullLed] = CRGB(255, 255, 0);
-//   }
-
-//   FastLED.show();
-// }
-
-// void getGameState()
-// {
-//   if (Serial.available() > 0)
-//   {
-//     String input = Serial.readStringUntil('\n');
-//     int separatorIndex = input.indexOf(',');
-//     if (separatorIndex != -1)
-//     {
-//       String healthStr = input.substring(0, separatorIndex);
-//       String ammoStr = input.substring(separatorIndex + 1);
-//       gameState.health = healthStr.toInt();
-//       gameState.ammo = ammoStr.toInt();
-//       Serial.print("Updated gameState - Health = ");
-//       Serial.print(gameState.health);
-//       Serial.print(", Ammo = ");
-//       Serial.println(gameState.ammo);
-//     }
-//   }
-// }
-
-// void sendImuData(imuData_t imuData, bool isMotion)
-// {
-//   if (imuWindowIndex < IMU_WINDOW_SIZE)
-//   {
-//     Serial.print("GyroXnorm:");
-//     Serial.print(imuData.gx);
-//     Serial.print(",");
-//     Serial.print(" GyroYnorm:");
-//     Serial.print(imuData.gy);
-//     Serial.print(",");
-//     Serial.print(" GyroZnorm:");
-//     Serial.print(imuData.gz);
-//     Serial.print(",");
-
-//     Serial.print("AccelXnorm:");
-//     Serial.print(imuData.ax);
-//     Serial.print(",");
-//     Serial.print(" AccelYnorm:");
-//     Serial.print(imuData.ay);
-//     Serial.print(",");
-//     Serial.print(" AccelZnorm:");
-//     Serial.println(imuData.az);
-
-//     imuDataArray[imuWindowIndex] = imuData;
-//     ++imuWindowIndex;
-
-//     if (imuWindowIndex == IMU_WINDOW_SIZE)
-//     {
-//       if (isMotionCounter > IMU_WINDOW_SIZE / 2) {
-//         // send imuDataArray
-//         Serial.println("imuDataArray is full, sending data...");
-//       } else {
-//         Serial.println("False motion");
-//       }
-//     }
-//   }
-//   else
-//   {
-//     // Serial.print("GyroXnorm:");
-//     // Serial.print(0);
-//     // Serial.print(",");
-//     // Serial.print(" GyroYnorm:");
-//     // Serial.print(0);
-//     // Serial.print(",");
-//     // Serial.print(" GyroZnorm:");
-//     // Serial.print(0);
-//     // Serial.print(",");
-
-//     // Serial.print("AccelXnorm:");
-//     // Serial.print(0);
-//     // Serial.print(",");
-//     // Serial.print(" AccelYnorm:");
-//     // Serial.print(0);
-//     // Serial.print(",");
-//     // Serial.print(" AccelZnorm:");
-//     // Serial.println(0);
-//   }
-// }
-
-// void loop()
-// {
-//   imuData_t imuData = getImuReadings();
-
-//   // mimic receiving gameState from game engine
-//   getGameState();
-
-//   isMotion = isMotionDetected2(imuData);
-//   if (imuWindowIndex == IMU_WINDOW_SIZE && isMotion)
-//   {
-//     imuWindowIndex = 0;
-//   }
-
-//   sendImuData(imuData, isMotion);
-
-//   // button logic
-//   shootAmmo();
-//   updateLed();
-//   Serial.println(digitalRead(BUTTON_PIN) == HIGH);
-
-//   // auto reload
-//   if (gameState.ammo == 0)
-//   {
-//     Serial.println("Reloading...");
-//     delay(1500);
-//     Serial.println("Done reloading");
-//     gameState.ammo = 6;
-//   }
-
-//   delay(50); // period = 50ms = 20Hz -> 20 samples per second
-// }
